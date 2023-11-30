19e40839c356cb3b9ac793c7fd6cf75089819048
dionex011 <code.testx11@gmail.com>
2023-11-30 16:33:50 +0530

    firstcommit

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 from sub import run_q
-cmd,fin=run_q("Number of employees")
-print(cmd)
-print(fin)
+import pandas as pd
+ 
+# import the StrinIO function
+# from io module
+from io import StringIO
+ 
+query="total number of employees"
+cmd,fin=run_q(query)
+# wrap the string data in StringIO function
+if type(cmd) is int:
+    print(type(cmd))
+else:
+    print(cmd.columns)
--- sub.py
+++ sub.py
@@ -11,7 +11,7 @@ from langchain.callbacks.manager import a  # debug get observation directly
 
 # Keys for OpenAI's API
 
-key = "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"
+key = "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"
 os.environ["OPENAI_API_KEY"] = key
 
 
@@ -48,7 +48,7 @@ cmd_out={cmd}
 
 
 def run_q(query: str):
-    agent = csv_to_df(OpenAI(temperature=0),
+    agent = csv_to_df(OpenAI(temperature=0,model='gpt-3.5-turbo-instruct'),
                       'uni.csv',
                       verbose=True)
 
